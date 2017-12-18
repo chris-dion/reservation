99e24421f6a89c3922da4ff3d604c9389b86fcda
chris-dion <michael.c.dion@gmail.com>
2017-12-18 11:47:25 -0600

    skeleton html and server

---
+++ server.js
@@ -0,0 +1,79 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// restaurant reservation (DATA)
+// =============================================================
+var reservation = [
+  {
+    name: "yoda",
+    phone: "777-777-7777",
+    email: "yoda@jedi.com",
+    id: 9000,
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Get all reservation
+app.get("/all", function(req, res) {
+  res.json(reservation);
+});
+
+// Search for Specific Character (or all reservation) - provides JSON
+app.get("/api/:reservation?", function(req, res) {
+  var chosen = req.params.reservation;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < reservation.length; i++) {
+      if (chosen === reservation[i].routeName) {
+        return res.json(reservation[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(reservation);
+});
+
+// Create New reservation - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  reservation.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
